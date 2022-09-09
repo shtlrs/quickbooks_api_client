ee7a88b22e1e7e485af3ae397f5860e7224e1899
Amrou Bellalouna <Amrou.bellalouna@expensya.com>
2022-09-09 16:20:40 +0100

    add conditions clean up utils

---
+++ builder/utils.py
@@ -0,0 +1,12 @@
+import textwrap
+
+
+def clean_up(string: str) -> str:
+    return textwrap.dedent(string.rstrip()).strip()
+
+
+def escape_characters(string):
+    string = clean_up(string)
+    string = string.translate(str.maketrans({"'": r"\'",
+                                             '"': r'\"'}))
+    return str(string)
