7e21001f3b2b9955c1e31d2580677f04174819ec
Amrou Bellalouna <Amrou.bellalouna@expensya.com>
2022-09-09 16:16:53 +0100

    add abstract conditions

---
+++ builder/conditions/abstract.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+from typing import Iterable, Union
+
+
+class AbstractCondition(ABC):
+    """
+    A base abstract class for all conditions
+    """
+    attribute: str
+    target: Union[Iterable, str, int]
+
+    def __init__(self, attribute: str):
+        self.attribute = attribute
+
+    @abstractmethod
+    def stringify(self) -> str:
+        """
+        Turns the condition into a string
+        """
